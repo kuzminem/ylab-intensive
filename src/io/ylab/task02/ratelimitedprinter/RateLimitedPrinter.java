59fb907944868c59204de87cea93c5b4d3eff2c0
Кузьмин Евгений <eugene.kuzmin@live.ru>
2023-03-12 14:47:10 +0300

    Rate Limited Printer

---
+++ src/io/ylab/task02/ratelimitedprinter/RateLimitedPrinter.java
@@ -0,0 +1,5 @@
+package io.ylab.task02.ratelimitedprinter;
+
+public interface RateLimitedPrinter {
+    void print(String message);
+}
---
+++ src/io/ylab/task02/ratelimitedprinter/RateLimitedPrinterImpl.java
@@ -0,0 +1,23 @@
+package io.ylab.task02.ratelimitedprinter;
+
+public class RateLimitedPrinterImpl implements RateLimitedPrinter {
+    private final int interval;
+    private long timer;
+
+    RateLimitedPrinterImpl(int interval) {
+        this.interval = interval;
+        setTimer();
+    }
+
+    private void setTimer() {
+        this.timer = System.currentTimeMillis() + this.interval;
+    }
+
+    @Override
+    public void print(String message) {
+        if (System.currentTimeMillis() > this.timer) {
+            System.out.println(message);
+            setTimer();
+        }
+    }
+}
