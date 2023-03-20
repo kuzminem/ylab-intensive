1c7085af6eeaba9c93e1e4b79581506939355c03
Кузьмин Евгений <eugene.kuzmin@live.ru>
2023-03-20 18:33:25 +0300

    task03

---
+++ src/io/ylab/task03/transliterator/Transliterator.java
@@ -0,0 +1,5 @@
+package io.ylab.task03.transliterator;
+
+public interface Transliterator {
+    String transliterate(String source);
+}
---
+++ src/io/ylab/task03/transliterator/TransliteratorImpl.java
@@ -0,0 +1,53 @@
+package io.ylab.task03.transliterator;
+
+import java.util.Map;
+
+import static java.util.Map.entry;
+
+public class TransliteratorImpl implements Transliterator {
+    private static final Map<Character, String> LETTER_MAP = Map.ofEntries(
+            entry('А', "A"),
+            entry('Б', "B"),
+            entry('В', "V"),
+            entry('Г', "G"),
+            entry('Д', "D"),
+            entry('Е', "E"),
+            entry('Ё', "E"),
+            entry('Ж', "ZH"),
+            entry('З', "Z"),
+            entry('И', "I"),
+            entry('Й', "I"),
+            entry('К', "K"),
+            entry('Л', "L"),
+            entry('М', "M"),
+            entry('Н', "N"),
+            entry('О', "O"),
+            entry('П', "P"),
+            entry('Р', "R"),
+            entry('С', "S"),
+            entry('Т', "T"),
+            entry('У', "U"),
+            entry('Ф', "F"),
+            entry('Х', "KH"),
+            entry('Ц', "TS"),
+            entry('Ч', "CH"),
+            entry('Ш', "SH"),
+            entry('Щ', "SHCH"),
+            entry('Ъ', "IE"),
+            entry('Ы', "Y"),
+            entry('Ь', ""),
+            entry('Э', "E"),
+            entry('Ю', "IU"),
+            entry('Я', "IA")
+    );
+
+    @Override
+    public String transliterate(String source) {
+        StringBuilder result = new StringBuilder();
+        for (char ch : source.toCharArray()) {
+            String symbol = String.valueOf(ch);
+            result.append(LETTER_MAP.getOrDefault(ch, symbol));
+        }
+        return result.toString();
+    }
+}
