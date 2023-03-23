d33d477e148d263ba18eda6d205fa22af2ee7422
Кузьмин Евгений <eugene.kuzmin@live.ru>
2023-03-23 14:59:34 +0300

    File Sorter

--- src/io/ylab/task03/filesort/Collector.java
+++ src/io/ylab/task03/filesort/Collector.java
@@ -1,47 +1,47 @@
 package io.ylab.task03.filesort;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Collector {
-    Collector(Separator separator) throws IOException {
-        final int filesNumber = separator.getFilesNumber();
-        final String folder = separator.getFolder();
+    private final List<ReadTempFile> readTempFiles = new ArrayList<>();
+    private final String sortedFileName;
 
-        TempFile[] tempFiles = new TempFile[filesNumber];
-        for (int i = 0; i < filesNumber; i++) {
-            tempFiles[i] = new TempFile(folder + i + ".txt");
+    Collector(Separator separator) throws IOException {
+        for (WriteTempFile f : separator.getWriteTempFiles()) {
+            this.readTempFiles.add(new ReadTempFile(f));
         }
 
+        this.sortedFileName = separator.getFolder() + "sorted.txt";
+
         try (BufferedWriter writer = new BufferedWriter(
-                new FileWriter(folder + "sorted.txt"))) {
+                new FileWriter(this.sortedFileName))) {
             for (int i = 0; i < separator.getSourceLines(); i++) {
-                int index = getIndex(tempFiles);
-                writer.write(tempFiles[index].getValue() + "\r\n");
-                tempFiles[index].read();
+                writer.write(collectValue() + "\r\n");
             }
         }
 
-        for (TempFile tempFile : tempFiles) {
-            tempFile.delete();
+        for (ReadTempFile f : readTempFiles) {
+            f.delete();
         }
     }
 
-    private int getIndex(TempFile[] tempFiles) {
-        int indexMin = 0;
-        for (int i = 0; i < tempFiles.length; i++) {
-            if (tempFiles[i].isReady()) {
-                indexMin = i;
-                break;
-            }
-        }
-        for (int i = 0; i < tempFiles.length; i++) {
-            if (tempFiles[i].isReady()
-                    && tempFiles[i].getValue() < tempFiles[indexMin].getValue()) {
-                indexMin = i;
-            }
-        }
-        return indexMin;
+    private Long collectValue() throws IOException {
+        ReadTempFile file = this.readTempFiles.stream()
+                .filter(ReadTempFile::isReady)
+                .min(Comparator.comparingLong(ReadTempFile::getValue))
+                .get();
+        long min = file.getValue();
+        file.read();
+        return min;
+    }
+
+    public File getSortedFile() {
+        return new File(this.sortedFileName);
     }
 }
--- src/io/ylab/task03/filesort/Separator.java
+++ src/io/ylab/task03/filesort/Separator.java
@@ -1,24 +1,21 @@
 package io.ylab.task03.filesort;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Separator {
     private final String folder;
-    private final int filesNumber;
     private final int sourceLines;
+    private final List<WriteTempFile> writeTempFiles = new ArrayList<>();
 
     Separator(File sourceFile, int filesNumber) throws IOException {
-        String path = sourceFile.getPath();
-        this.folder = path.substring(0, path.lastIndexOf('\\') + 1);
-        this.filesNumber = filesNumber;
+        this.folder = sourceFile.getPath()
+                .substring(0, sourceFile.getPath().lastIndexOf('\\') + 1);
+
         int counter = 0;
         try (BufferedReader reader = new BufferedReader(new FileReader(sourceFile))) {
             while (reader.readLine() != null) {
@@ -28,37 +25,26 @@ public class Separator {
         this.sourceLines = counter;
 
         try (BufferedReader reader = new BufferedReader(new FileReader(sourceFile))) {
-            int targetLines = this.sourceLines / this.filesNumber;
+            int targetLines = this.sourceLines / filesNumber;
             int capacity = (int) (targetLines * 1.4);
 
-            for (int i = 0; i < this.filesNumber - 1; i++) {
-                List<Long> lines = new ArrayList<>(capacity);
+            for (int i = 0; i < filesNumber - 1; i++) {
+                List<Long> longList = new ArrayList<>(capacity);
                 for (int j = 0; j < targetLines; j++) {
                     String line = reader.readLine();
-                    lines.add(Long.parseLong(line));
+                    longList.add(Long.parseLong(line));
                 }
-                Collections.sort(lines);
-                try (BufferedWriter writer = new BufferedWriter(
-                        new FileWriter(this.folder + i + ".txt"))) {
-                    for (Long l : lines) {
-                        writer.write(l + "\r\n");
-                    }
-                }
+                this.writeTempFiles.add(new WriteTempFile(this.folder + i + ".txt", longList));
             }
 
-            List<Long> lines = new ArrayList<>(capacity);
-            int lastTargetLines = this.sourceLines - targetLines * (this.filesNumber - 1);
+            List<Long> longList = new ArrayList<>(capacity);
+            int lastTargetLines = this.sourceLines - targetLines * (filesNumber - 1);
             for (int j = 0; j < lastTargetLines; j++) {
                 String line = reader.readLine();
-                lines.add(Long.parseLong(line));
+                longList.add(Long.parseLong(line));
             }
-            Collections.sort(lines);
-            try (BufferedWriter writer = new BufferedWriter(
-                    new FileWriter(this.folder + (this.filesNumber - 1) + ".txt"))) {
-                for (Long l : lines) {
-                    writer.write(l + "\r\n");
-                }
-            }
+            this.writeTempFiles.add(
+                    new WriteTempFile(this.folder + (filesNumber - 1) + ".txt", longList));
         }
     }
 
@@ -66,11 +52,11 @@ public class Separator {
         return this.folder;
     }
 
-    public int getFilesNumber() {
-        return filesNumber;
-    }
-
     public int getSourceLines() {
         return this.sourceLines;
+    }
+
+    public List<WriteTempFile> getWriteTempFiles() {
+        return this.writeTempFiles;
     }
 }
