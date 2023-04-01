1a0ea18829ef2b75385648e5cf83762c41a826ce
Кузьмин Евгений <eugene.kuzmin@live.ru>
2023-04-01 12:14:49 +0300

    Lesson 5

---
+++ lesson05/src/main/java/io/ylab/intensive/lesson05/eventsourcing/db/Order.java
@@ -0,0 +1,32 @@
+package io.ylab.intensive.lesson05.eventsourcing.db;
+
+import io.ylab.intensive.lesson05.eventsourcing.Person;
+
+public class Order {
+    private String command;
+    private Person person;
+
+    public Order() {
+    }
+
+    public Order(String command, Person person) {
+        this.command = command;
+        this.person = person;
+    }
+
+    public String getCommand() {
+        return this.command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public Person getPerson() {
+        return this.person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+}
