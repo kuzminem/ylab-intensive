f1e6df607d489b30e6b5c8c60e1a89bef281f2a8
Кузьмин Евгений <eugene.kuzmin@live.ru>
2023-04-04 11:28:45 +0300

    Message Filter

--- lesson05/src/main/java/io/ylab/intensive/lesson05/messagefilter/Censor.java
+++ lesson05/src/main/java/io/ylab/intensive/lesson05/messagefilter/Censor.java
@@ -25,6 +25,7 @@ public class Censor {
     @Autowired
     public Censor(DataSource dataSource) throws SQLException {
         this.connection = dataSource.getConnection();
+
         DatabaseMetaData metaData = this.connection.getMetaData();
         ResultSet resultSet = metaData
                 .getTables(null, "%", "censor", new String[]{"TABLE"});
@@ -33,7 +34,38 @@ public class Censor {
         }
     }
 
-    public boolean isBad(String word) {
+    public String correct(String input) {
+        StringBuilder mask = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            String letter = input.substring(i, i + 1);
+            mask.append(letter.matches("[a-zA-Zа-яА-Я0-9ёЁ_]") ? letter : " ");
+        }
+
+        String[] words = mask.toString().split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() > 2 && isBad(words[i])) {
+                words[i] = words[i].charAt(0)
+                        + "*".repeat(words[i].length() - 2)
+                        + words[i].charAt(words[i].length() - 1);
+            }
+        }
+
+        StringBuilder output = new StringBuilder();
+        int wordsCounter = 0;
+        for (int i = 0; i < mask.length(); i++) {
+            if (mask.charAt(i) == ' ') {
+                output.append(input.charAt(i));
+            } else {
+                output.append(words[wordsCounter]);
+                i += words[wordsCounter].length() - 1;
+                wordsCounter++;
+            }
+        }
+
+        return output.toString();
+    }
+
+    private boolean isBad(String word) {
         try {
             String selectQuery = "select count(*) "
                     + "from censor "
--- lesson05/src/main/java/io/ylab/intensive/lesson05/messagefilter/MessageFilterApp.java
+++ lesson05/src/main/java/io/ylab/intensive/lesson05/messagefilter/MessageFilterApp.java
@@ -1,16 +1,50 @@
 package io.ylab.intensive.lesson05.messagefilter;
 
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.GetResponse;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
 public class MessageFilterApp {
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(Config.class);
+    public static void main(String[] args) throws IOException, TimeoutException {
+        AnnotationConfigApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(Config.class);
         applicationContext.start();
 
-        Responder responder = applicationContext.getBean(Responder.class);
+        ConnectionFactory connectionFactory =
+                applicationContext.getBean(ConnectionFactory.class);
+        Connection connection = connectionFactory.newConnection();
 
-        String input = "Оп-па, оп-па, у Джигурды большая жопа!";
-        System.out.println(input);
-        System.out.println(responder.getOutput(input));
+        final String exchangeName = "exc";
+
+        final String inputQueueName = "input";
+        Channel inputChannel = connection.createChannel();
+        inputChannel.exchangeDeclare(exchangeName, BuiltinExchangeType.TOPIC);
+        inputChannel.queueDeclare(inputQueueName, true, false, false, null);
+        inputChannel.queueBind(inputQueueName, exchangeName, "*");
+
+        final String outputQueueName = "output";
+        Channel outputChannel = connection.createChannel();
+        outputChannel.exchangeDeclare(exchangeName, BuiltinExchangeType.TOPIC);
+        outputChannel.queueDeclare(outputQueueName, true, false, false, null);
+        outputChannel.queueBind(outputQueueName, exchangeName, "*");
+
+        Censor censor = applicationContext.getBean(Censor.class);
+
+        while (!Thread.currentThread().isInterrupted()) {
+            GetResponse response = inputChannel.basicGet(inputQueueName, true);
+            if (response != null) {
+                byte[] body = response.getBody();
+                String input = new String(body, StandardCharsets.UTF_8);
+                String output = censor.correct(input);
+                outputChannel.basicPublish(exchangeName, "*", null, output.getBytes(StandardCharsets.UTF_8));
+            }
+        }
     }
 }
